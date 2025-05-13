099dd8a64e907cc7866c76eb672503849a123837
stofengli-admin <aaa150964@163.com>
2025-05-13 22:06:32 +0800

    abcd

--- Tools/MeshBuilder.cs
+++ Tools/MeshBuilder.cs
@@ -8,11 +8,11 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.UIElements;
 
-public static class MeshBuilder 
+public static class MeshBuilder
 {
     public static void AddTriangles(SimpleMesh simpleMesh, int vertex1, int vertex2, int vertex3)
     {
-        if(vertex1 >= simpleMesh.vertices.Count || 
+        if (vertex1 >= simpleMesh.vertices.Count ||
            vertex2 >= simpleMesh.vertices.Count ||
            vertex3 >= simpleMesh.vertices.Count)
         {
@@ -21,7 +21,7 @@ public static class MeshBuilder
         }
         simpleMesh.TriangleIndexs.Add(vertex1);
         simpleMesh.TriangleIndexs.Add(vertex2);
-        simpleMesh.TriangleIndexs.Add(vertex3); 
+        simpleMesh.TriangleIndexs.Add(vertex3);
     }
     public static SimpleMesh SimpleMeshPretreatmentSQ(SimpleMesh simpleMesh)
     {
@@ -29,8 +29,8 @@ public static class MeshBuilder
         {
             Debug.LogError("The value to define the shape of the mesh should be nonnegative in the simple create mode!");
             return null;
-        }   
-        if(simpleMesh.row <= 1 || simpleMesh.column <=1 )
+        }
+        if (simpleMesh.row <= 1 || simpleMesh.column <= 1)
         {
             Debug.LogError("The mesh's row or column must be greater than 1!");
             return null;
@@ -46,36 +46,36 @@ public static class MeshBuilder
         Vector3 position = go.transform.position;
         Vector3 nextPosition;
 
-        dx = simpleMesh.length * 2 / (simpleMesh.column - 1);
-        dy = simpleMesh.width * 2 / (simpleMesh.row - 1);
-        for(int i = 0; i < simpleMesh.row ; i++)//uv(0,0)
+        dx = simpleMesh.length * 2 / (simpleMesh.row - 1);
+        dy = simpleMesh.width * 2 / (simpleMesh.column - 1);
+        for (int i = 0; i < simpleMesh.column; i++)//uv(0,0)
         {
             //move to i column
-            float v = i * (1.0f / (float)(simpleMesh.row - 1));
+            float v = i * (1.0f / (float)(simpleMesh.column - 1));
 
             nextPosition = position + new Vector3(-simpleMesh.length, -simpleMesh.width, 0);
             nextPosition = nextPosition + new Vector3(0, i * dy, 0);
-            for (int j = 0; j < simpleMesh.column; j++)
+            for (int j = 0; j < simpleMesh.row; j++)
             {
                 simpleMesh.vertices.Add(nextPosition);
                 nextPosition = nextPosition + new Vector3(dx, 0, 0);
 
-                float u = j * (1.0f / (float)(simpleMesh.column - 1));
+                float u = j * (1.0f / (float)(simpleMesh.row - 1));
                 simpleMesh.uv.Add(new Vector2(u, v));
             }
         }
 
-        for (int i = 0; i < simpleMesh.row - 1; i++)
+        for (int i = 0; i < simpleMesh.column - 1; i++)
         {
-            for (int j = 0; j < simpleMesh.column - 1; j++)
+            for (int j = 0; j < simpleMesh.row - 1; j++)
             {
-                simpleMesh.TriangleIndexs.Add(i * simpleMesh.column + j);
-                simpleMesh.TriangleIndexs.Add((i + 1) * simpleMesh.column + j);
-                simpleMesh.TriangleIndexs.Add(i * simpleMesh.column + j + 1);
+                simpleMesh.TriangleIndexs.Add(i * simpleMesh.row + j);
+                simpleMesh.TriangleIndexs.Add((i + 1) * simpleMesh.row + j);
+                simpleMesh.TriangleIndexs.Add(i * simpleMesh.row + j + 1);
 
-                simpleMesh.TriangleIndexs.Add((i + 1) * simpleMesh.column + j + 1);
-                simpleMesh.TriangleIndexs.Add(i * simpleMesh.column + j + 1);
-                simpleMesh.TriangleIndexs.Add((i + 1) * simpleMesh.column + j);
+                simpleMesh.TriangleIndexs.Add((i + 1) * simpleMesh.row + j + 1);
+                simpleMesh.TriangleIndexs.Add(i * simpleMesh.row + j + 1);
+                simpleMesh.TriangleIndexs.Add((i + 1) * simpleMesh.row + j);
             }
         }
 
@@ -104,36 +104,36 @@ public static class MeshBuilder
         Vector3 position = go.transform.position;
         Vector3 nextPosition;
 
-        dx = simpleMesh.length * 2 / (simpleMesh.column - 1);
-        dz = simpleMesh.width * 2 / (simpleMesh.row - 1);
-        for (int i = 0; i < simpleMesh.row; i++)
+        dx = simpleMesh.length * 2 / (simpleMesh.row - 1);
+        dz = simpleMesh.width * 2 / (simpleMesh.column - 1);
+        for (int i = 0; i < simpleMesh.column; i++)
         {
             //move to i column
-            float v = i * (1.0f / (float)(simpleMesh.row - 1));
+            float v = i * (1.0f / (float)(simpleMesh.column - 1));
 
             nextPosition = position + new Vector3(-simpleMesh.length, 0, -simpleMesh.width);
             nextPosition = nextPosition + new Vector3(0, 0, i * dz);
-            for (int j = 0; j < simpleMesh.column; j++)
+            for (int j = 0; j < simpleMesh.row; j++)
             {
                 simpleMesh.vertices.Add(nextPosition);
                 nextPosition = nextPosition + new Vector3(dx, 0, 0);
 
-                float u = j * (1.0f / (float)(simpleMesh.column - 1));
+                float u = j * (1.0f / (float)(simpleMesh.row - 1));
                 simpleMesh.uv.Add(new Vector2(u, v));
             }
         }
 
-        for (int i = 0; i < simpleMesh.row - 1; i++)
+        for (int i = 0; i < simpleMesh.column - 1; i++)
         {
-            for (int j = 0; j < simpleMesh.column - 1; j++)
+            for (int j = 0; j < simpleMesh.row - 1; j++)
             {
-                simpleMesh.TriangleIndexs.Add(i * simpleMesh.column + j);
-                simpleMesh.TriangleIndexs.Add((i + 1) * simpleMesh.column + j);
-                simpleMesh.TriangleIndexs.Add(i * simpleMesh.column + j + 1);
+                simpleMesh.TriangleIndexs.Add(i * simpleMesh.row + j);
+                simpleMesh.TriangleIndexs.Add((i + 1) * simpleMesh.row + j);
+                simpleMesh.TriangleIndexs.Add(i * simpleMesh.row + j + 1);
 
-                simpleMesh.TriangleIndexs.Add((i + 1) * simpleMesh.column + j + 1);
-                simpleMesh.TriangleIndexs.Add(i * simpleMesh.column + j + 1);
-                simpleMesh.TriangleIndexs.Add((i + 1) * simpleMesh.column + j);
+                simpleMesh.TriangleIndexs.Add((i + 1) * simpleMesh.row + j + 1);
+                simpleMesh.TriangleIndexs.Add(i * simpleMesh.row + j + 1);
+                simpleMesh.TriangleIndexs.Add((i + 1) * simpleMesh.row + j);
             }
         }
 
@@ -143,7 +143,7 @@ public static class MeshBuilder
     public static SimpleMesh CreateMesh(SimpleMesh simpleMesh)
     {
         simpleMesh.mesh = new Mesh();
-        simpleMesh.mesh.vertices = simpleMesh.vertices.ToArray(); 
+        simpleMesh.mesh.vertices = simpleMesh.vertices.ToArray();
         simpleMesh.mesh.triangles = simpleMesh.TriangleIndexs.ToArray();
         simpleMesh.mesh.uv = simpleMesh.uv.ToArray();
 
